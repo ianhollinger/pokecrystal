c93b4eef80475b41eb2944c97b71e9566d12134e
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2023-08-13 20:33:41 -0400

    Update ElmsLab.asm
    replaced everstone with exp. share and added descriptive text

--- maps/ElmsLab.asm
+++ maps/ElmsLab.asm
@@ -96,14 +96,14 @@ ProfElmScript:
 	iftrue ElmGiveTicketScript
 ElmCheckMasterBall:
 	checkevent EVENT_GOT_MASTER_BALL_FROM_ELM
-	iftrue ElmCheckEverstone
+	iftrue ElmCheckExpShare
 	checkflag ENGINE_RISINGBADGE
 	iftrue ElmGiveMasterBallScript
-ElmCheckEverstone:
-	checkevent EVENT_GOT_EVERSTONE_FROM_ELM
+ElmCheckExpShare:
+	checkevent EVENT_GOT_EXP_SHARE_FROM_ELM
 	iftrue ElmScript_CallYou
 	checkevent EVENT_SHOWED_TOGEPI_TO_ELM
-	iftrue ElmGiveEverstoneScript
+	iftrue ElmGiveExpShareScript
 	checkevent EVENT_TOLD_ELM_ABOUT_TOGEPI_OVER_THE_PHONE
 	iffalse ElmCheckTogepiEgg
 	setval TOGEPI
@@ -378,21 +378,21 @@ ShowElmTogepiScript:
 	promptbutton
 	writetext ShowElmTogepiText3
 	promptbutton
-ElmGiveEverstoneScript:
-	writetext ElmGiveEverstoneText1
+ElmGiveExpShareScript:
+	writetext ElmGiveExpShareText1
 	promptbutton
-	verbosegiveitem EVERSTONE
-	iffalse ElmScript_NoRoomForEverstone
-	writetext ElmGiveEverstoneText2
+	verbosegiveitem EXP_SHARE
+	iffalse ElmScript_NoRoomForExpShare
+	writetext ElmGiveExpShareText2
 	waitbutton
 	closetext
-	setevent EVENT_GOT_EVERSTONE_FROM_ELM
+	setevent EVENT_GOT_EXP_SHARE_FROM_ELM
 	end
 
 ElmScript_CallYou:
 	writetext ElmText_CallYou
 	waitbutton
-ElmScript_NoRoomForEverstone:
+ElmScript_NoRoomForExpShare:
 	closetext
 	end
 
@@ -1095,7 +1095,7 @@ ShowElmTogepiText3:
 	cont "to be done."
 	done
 
-ElmGiveEverstoneText1:
+ElmGiveExpShareText1:
 	text "Thanks, <PLAY_G>!"
 	line "You're helping"
 
@@ -1107,23 +1107,21 @@ ElmGiveEverstoneText1:
 	cont "our appreciation."
 	done
 
-ElmGiveEverstoneText2:
+ElmGiveExpShareText2:
 	text "That's an"
-	line "EVERSTONE."
+	line "EXP. SHARE."
 
-	para "Some species of"
-	line "#MON evolve"
+	para "If you turn it on,"
+	line "experience gained"
 
-	para "when they grow to"
-	line "certain levels."
+	para "in battle will be"
+	line "shared with all"
 
-	para "A #MON holding"
-	line "the EVERSTONE"
-	cont "won't evolve."
-
-	para "Give it to a #-"
-	line "MON you don't want"
-	cont "to evolve."
+	para "#MON in your"
+	line "party, even if"
+	
+	para "they didn't"
+	line "participate."
 	done
 
 ElmText_CallYou:
