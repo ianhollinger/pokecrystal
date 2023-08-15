f61234056be7fd8633e24d3de54c66087c51bc9a
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2023-08-14 22:45:23 -0400

    Update treemons_asleep.asm
    switched ledyba and spinarak cause they're night and day mons now, respectively

--- data/wild/treemons_asleep.asm
+++ data/wild/treemons_asleep.asm
@@ -10,22 +10,25 @@ AsleepTreeMonsNite:
 	db SPEAROW
 	db EKANS
 	db EXEGGCUTE
-	db LEDYBA
 	db AIPOM
+	db SPINARAK
+	db ARIADOS
 	db -1 ; end
 
 AsleepTreeMonsDay:
 	db VENONAT
 	db HOOTHOOT
 	db NOCTOWL
-	db SPINARAK
 	db HERACROSS
+	db LEDYBA
+	db LEDIAN
 	db -1 ; end
 
 AsleepTreeMonsMorn:
 	db VENONAT
 	db HOOTHOOT
 	db NOCTOWL
-	db SPINARAK
+	db LEDYBA
+	db LEDIAN
 	db HERACROSS
 	db -1 ; end
