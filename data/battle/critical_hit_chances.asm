bfeff3b16071733c15719a3d2e043bd9c808dbea
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2023-08-24 23:25:28 -0400

    Update critical_hit_chances.asm
    let's see if this works

--- data/battle/critical_hit_chances.asm
+++ data/battle/critical_hit_chances.asm
@@ -1,8 +1,8 @@
 CriticalHitChances:
-	db 1 out_of 15 ;  0
+	db 1 out_of 16 ;  0
 	db 1 out_of 8  ; +1
 	db 1 out_of 4  ; +2
 	db 1 out_of 2  ; +3
-	db 1 out_of 2  ; +4
-	db 1 out_of 2  ; +5
-	db 1 out_of 2  ; +6
+	db 1 out_of 1  ; +4
+	db 1           ; +5
+	db 1           ; +6
