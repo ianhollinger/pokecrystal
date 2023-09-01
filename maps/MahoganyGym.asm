9c7ec2189506068253e5def9d6f169191ae12349
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2023-09-01 16:20:19 -0400

    Update MahoganyGym.asm
    I thought I changed that

--- maps/MahoganyGym.asm
+++ maps/MahoganyGym.asm
@@ -198,14 +198,12 @@ Text_ReceivedGlacierBadge:
 	done
 
 PryceText_GlacierBadgeSpeech:
-	text "That BADGE will"
-	line "raise the SPECIAL"
-	cont "stats of #MON."
+	text "That BADGE lets"
+	line "your #MON use"
+	cont "WHIRLPOOL to get"
 
-	para "It also lets your"
-	line "#MON use WHIRL-"
-	cont "POOL to get across"
-	cont "real whirlpools."
+	para "across real"
+	line "whirlpools."
 
 	para "And this… This is"
 	line "a gift from me!"
