dd189cac7fc7bc432a402470415724ad292aec0a
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2023-09-10 15:55:40 -0400

    Update type_matchups.asm
    it's not doing anything

--- data/types/type_matchups.asm
+++ data/types/type_matchups.asm
@@ -93,7 +93,6 @@ TypeMatchups:
 	db GHOST,        NORMAL,       NO_EFFECT
 	db GHOST,        PSYCHIC_TYPE, SUPER_EFFECTIVE
 	db GHOST,        DARK,         NOT_VERY_EFFECTIVE
-;	db GHOST,        STEEL,        NOT_VERY_EFFECTIVE
 	db GHOST,        GHOST,        SUPER_EFFECTIVE
 	db DRAGON,       DRAGON,       SUPER_EFFECTIVE
 	db DRAGON,       STEEL,        NOT_VERY_EFFECTIVE
@@ -101,7 +100,6 @@ TypeMatchups:
 	db DARK,         PSYCHIC_TYPE, SUPER_EFFECTIVE
 	db DARK,         GHOST,        SUPER_EFFECTIVE
 	db DARK,         DARK,         NOT_VERY_EFFECTIVE
-;	db DARK,         STEEL,        NOT_VERY_EFFECTIVE
 	db STEEL,        FIRE,         NOT_VERY_EFFECTIVE
 	db STEEL,        WATER,        NOT_VERY_EFFECTIVE
 	db STEEL,        ELECTRIC,     NOT_VERY_EFFECTIVE
