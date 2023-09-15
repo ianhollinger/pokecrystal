02951408736a5a9a29efa4de2f86d6d1e6a7560d
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2023-09-15 11:36:45 -0400

    Update rooftop_sale.asm
    25% off Everything must go

--- data/items/rooftop_sale.asm
+++ data/items/rooftop_sale.asm
@@ -1,17 +1,17 @@
 RooftopSaleMart1:
 	db 5
 	dbw POKE_BALL,     150
-	dbw GREAT_BALL,    500
-	dbw SUPER_POTION,  500
-	dbw FULL_HEAL,     500
-	dbw REVIVE,       1200
+	dbw GREAT_BALL,    450
+	dbw SUPER_POTION,  525
+	dbw FULL_HEAL,     300
+	dbw REVIVE,       1500
 	db -1
 
 RooftopSaleMart2:
 	db 5
-	dbw HYPER_POTION, 1000
-	dbw FULL_RESTORE, 2000
-	dbw FULL_HEAL,     500
-	dbw ULTRA_BALL,   1000
-	dbw PROTEIN,      7800
+	dbw HYPER_POTION,  900
+	dbw FULL_RESTORE, 2250
+	dbw FULL_HEAL,     300
+	dbw ULTRA_BALL,    900
+	dbw HP_UP,        7350
 	db -1
