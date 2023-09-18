2b1e0f2e9a004c2135930c0efca8b445083ed318
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2023-09-18 00:01:28 -0400

    Update GoldenrodGameCorner.asm
    also Y + 3

--- maps/GoldenrodGameCorner.asm
+++ maps/GoldenrodGameCorner.asm
@@ -282,7 +282,7 @@ GoldenrodGameCornerPrizeMonVendorScript:
 
 .MenuHeader:
 	db MENU_BACKUP_TILES ; flags
-	menu_coords 0, 2, 17, TEXTBOX_Y - 1
+	menu_coords 0, 2, 17, TEXTBOX_Y + 3
 	dw .MenuData
 	db 1 ; default option
 
