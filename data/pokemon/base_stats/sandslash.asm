ff12c5ec706ed0f6ef5d4baff2135e6427b2fc31
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2023-09-29 16:33:51 -0400

    Update sandslash.asm
    the good Lord giveth soft sand, the good Lord taketh away soft sand

--- data/pokemon/base_stats/sandslash.asm
+++ data/pokemon/base_stats/sandslash.asm
@@ -6,7 +6,7 @@
 	db GROUND, GROUND ; type
 	db 90 ; catch rate
 	db 158 ; base exp
-	db SOFT_SAND, QUICK_CLAW ; items
+	db NO_ITEM, QUICK_CLAW ; items
 	db GENDER_F50 ; gender ratio
 	db 100 ; unknown 1
 	db 20 ; step cycles to hatch
