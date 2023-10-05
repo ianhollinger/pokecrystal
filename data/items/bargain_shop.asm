bc9a92ebfcbae39fc4c8b8fdf1724aec4d66afb9
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2023-10-05 01:58:28 -0400

    Update bargain_shop.asm
    All are 0.9x normal sell price

--- data/items/bargain_shop.asm
+++ data/items/bargain_shop.asm
@@ -1,8 +1,8 @@
 BargainShopData:
 	db 5
 	dbw NUGGET,     9000
-	dbw PEARL,       650
-	dbw BIG_PEARL,  3500
-	dbw STARDUST,    900
-	dbw STAR_PIECE, 4600
+	dbw PEARL,       900
+	dbw BIG_PEARL,  3600
+	dbw STARDUST,   1350
+	dbw STAR_PIECE, 4500
 	db -1
