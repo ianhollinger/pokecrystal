8c4b53053813ef407947d189db5a98272dfe6a8a
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2023-10-06 01:31:34 -0400

    Update engine_flags.asm
    union cave spawn

--- data/events/engine_flags.asm
+++ data/events/engine_flags.asm
@@ -99,7 +99,7 @@ EngineFlags:
 	engine_flag wVisitedSpawns, SPAWN_LAKE_OF_RAGE
 	engine_flag wVisitedSpawns, SPAWN_BLACKTHORN
 	engine_flag wVisitedSpawns, SPAWN_MT_SILVER
-	engine_flag wVisitedSpawns, NUM_SPAWNS ; unused
+	engine_flag wVisitedSpawns, SPAWN_UNION_CAVE
 
 	engine_flag wLuckyNumberShowFlag, LUCKYNUMBERSHOW_GAME_OVER_F
 
