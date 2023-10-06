d278f5326c5d54e5f240944848ff772756173894
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2023-10-06 11:54:10 -0400

    Update ElmsLab.asm
    Putting the trigger for the slowpoke guy to offer you tail to after the aide gives you poke balls

--- maps/ElmsLab.asm
+++ maps/ElmsLab.asm
@@ -347,6 +347,7 @@ ElmAfterTheftScript:
 	waitbutton
 	closetext
 	setscene SCENE_ELMSLAB_AIDE_GIVES_POKE_BALLS
+        setmapscene ROUTE_32, SCENE_ROUTE32_OFFER_SLOWPOKETAIL
 	end
 
 ElmStudyingEggScript:
