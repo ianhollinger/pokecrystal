f222b59e4b9db42e0c353da5ca703ddf15c450dd
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2023-10-10 12:08:04 -0400

    Update SeafoamGym.asm

--- maps/SeafoamGym.asm
+++ maps/SeafoamGym.asm
@@ -43,7 +43,7 @@ SeafoamGymBlaineScript:
 	iftrue .FightDone
 	checkevent EVENT_OPENED_MT_SILVER
 	iftrue .Rematch
-	iffalse .FightDone
+	call .FightDone
 
 .Rematch:
 	writetext BlaineRematchIntroText
