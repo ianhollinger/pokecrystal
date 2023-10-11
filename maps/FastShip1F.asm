9ffb30804779bb8e66c423976b94bf2ebacbbfcc
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2023-10-10 23:08:34 -0400

    Update FastShip1F.asm

--- maps/FastShip1F.asm
+++ maps/FastShip1F.asm
@@ -187,13 +187,13 @@ FastShip1F_PlayerHitByGrandpaMovement:
 	turn_head LEFT
 	step_end
 
-FastShip1F_StepUpMovement: ; unreferenced
-	step UP
-	step_end
+; FastShip1F_StepUpMovement: ; unreferenced
+;	step UP
+;	step_end
 
-FastShip1F_StepDownMovement: ; unreferenced
-	step DOWN
-	step_end
+; FastShip1F_StepDownMovement: ; unreferenced
+;	step DOWN
+;	step_end
 
 FastShip1F_PlayerLeavesShipMovement:
 	step UP
