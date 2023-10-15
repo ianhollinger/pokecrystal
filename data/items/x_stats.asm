d438bd1a1adb080abde0bf2a0d4b33b9f548d26c
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2023-10-14 21:01:11 -0400

    Update x_stats.asm
    Trying something i don't understand

--- data/items/x_stats.asm
+++ data/items/x_stats.asm
@@ -1,7 +1,7 @@
 XItemStats:
 	;  item,      stat
-	db X_ATTACK,  ATTACK
-	db X_DEFEND,  DEFENSE
-	db X_SPEED,   SPEED
-	db X_SPECIAL, SP_ATTACK
-	db X_SP_DEF,  SP_DEFENSE
+	db X_ATTACK,  $10 | ATTACK
+	db X_DEFEND,  $10 | DEFENSE
+	db X_SPEED,   $10 | SPEED
+	db X_SPECIAL, $10 | SP_ATTACK
+	db X_SP_DEF,  $10 | SP_DEFENSE
