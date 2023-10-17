35211787641a5cc1f865362a3b35e92e733a1180
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2023-10-16 23:12:54 -0400

    Update VioletGym.asm

--- maps/VioletGym.asm
+++ maps/VioletGym.asm
@@ -11,9 +11,9 @@ VioletGym_MapScripts:
 
 VioletGymFalknerScript:
 	faceplayer
-        opentext
 	checkevent EVENT_BEAT_FALKNER
 	iftrue .PostGame
+        opentext
 	writetext FalknerIntroText
 	waitbutton
 	closetext
@@ -49,6 +49,7 @@ VioletGymFalknerScript:
 	iftrue .SpeechAfterRematch
         checkevent EVENT_GOT_TM31_MUD_SLAP
 	iftrue .SpeechAfterTM
+        opentext
 	setevent EVENT_BEAT_BIRD_KEEPER_ROD
 	setevent EVENT_BEAT_BIRD_KEEPER_ABE
 	setmapscene ELMS_LAB, SCENE_ELMSLAB_NOOP
@@ -64,6 +65,7 @@ VioletGymFalknerScript:
 	end
 
 .SpeechAfterTM:
+        opentext
 	writetext FalknerFightDoneText
 	waitbutton
 .NoRoomForMudSlap:
