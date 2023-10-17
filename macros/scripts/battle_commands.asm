f1506bb264dc415a0075b0270aba46cfbe1e1481
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2023-10-17 17:22:06 -0400

    Update battle_commands.asm

--- macros/scripts/battle_commands.asm
+++ macros/scripts/battle_commands.asm
@@ -180,8 +180,8 @@ ENDM
 	command supereffectivelooptext  ; ad
 	command startloop               ; ae
 	command curl                    ; af
-;	command growth			; b0
-;	command lowkick			; b1
+	command growth			; b0
+	command lowkick			; b1
 DEF NUM_EFFECT_COMMANDS EQU const_value - 1
 
 	const_def -1, -1
