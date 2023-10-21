67329a513b053d5f905dc2b7d6abb23a03d86fbd
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2023-10-20 21:48:24 -0400

    Update critical_hit_chances.asm
    Getting 1/4 crit rate on something that should be 100%

--- data/battle/critical_hit_chances.asm
+++ data/battle/critical_hit_chances.asm
@@ -3,6 +3,6 @@ CriticalHitChances:
 	db 1 out_of 8  ; +1
 	db 1 out_of 4  ; +2
 	db 1 out_of 2  ; +3
-	db 1           ; +4
-	db 1           ; +5
-	db 1           ; +6
+	db 254 out_of 255 ; +4
+	db 254 out_of 255 ; +5
+	db 254 out_of 255 ; +6
