47d5b825090fa40ec9bf4226ca68c8d03bf71655
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2023-10-27 15:28:20 -0400

    Update VioletPokecenter1F.asm

--- maps/VioletPokecenter1F.asm
+++ maps/VioletPokecenter1F.asm
@@ -30,6 +30,8 @@ VioletPokecenter1F_ElmsAideScript:
 	setevent EVENT_GOT_TOGEPI_EGG_FROM_ELMS_AIDE
 	clearevent EVENT_ELMS_AIDE_IN_LAB
 	clearevent EVENT_TOGEPI_HATCHED
+	clearevent EVENT_SLOWPOKE_WELL_ROCKETS
+	setmapscene ROUTE_32, SCENE_ROUTE32_OFFER_SLOWPOKETAIL
 	writetext VioletPokecenterElmsAideGiveEggText
 	waitbutton
 	closetext
