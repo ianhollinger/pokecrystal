565a9957a088cf078b65fcbf3a141e42d582db78
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2023-11-01 09:45:03 -0400

    Update blocks.asm

--- data/maps/blocks.asm
+++ data/maps/blocks.asm
@@ -950,8 +950,8 @@ VermilionPortPassage_Blocks:
 FuchsiaGym_Blocks:
 	INCBIN "maps/FuchsiaGym.blk"
 
-SafariZoneBeta_Blocks:
-	INCBIN "maps/SafariZoneBeta.blk"
+; SafariZoneBeta_Blocks:
+;	INCBIN "maps/SafariZoneBeta.blk"
 
 UndergroundPath_Blocks:
 	INCBIN "maps/UndergroundPath.blk"
