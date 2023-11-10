dbbd984f3d4e4783d1feb217c5da3c28e6e3ff91
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2023-11-10 18:57:54 -0500

    Update BlackthornCity.asm
    move reminder sign

--- maps/BlackthornCity.asm
+++ maps/BlackthornCity.asm
@@ -290,9 +290,9 @@ BlackthornGymSignText:
 	done
 
 MoveDeletersHouseSignText:
-	text "MOVE DELETER'S"
-	line "HOUSE"
-	done
+	text "MOVE DELETER AND"
+	line "MOVE REMINDER'S"
+	cont "HOUSE"
 
 DragonDensSignText:
 	text "DRAGON'S DEN"
