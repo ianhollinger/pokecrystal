192cc363c58061d9cf73a5627acf7b89ed516ebf
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2023-11-15 19:41:47 -0500

    Update pokedex.asm

--- engine/pokedex/pokedex.asm
+++ engine/pokedex/pokedex.asm
@@ -1164,8 +1164,8 @@ Pokedex_DrawDexEntryScreenBG:
 	call Pokedex_PlaceFrontpicTopLeftCorner
 	ret
 
-.Number: ; unreferenced
-	db $5c, $5d, -1 ; No.
+; .Number: ; unreferenced
+;	db $5c, $5d, -1 ; No.
 .Height:
 	db "HT  ?", $5e, "??", $5f, -1 ; HT  ?'??"
 .Weight:
