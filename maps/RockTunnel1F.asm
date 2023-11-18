69f5246e9faeb37574e988179fdd3cf986143271
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2023-11-17 21:19:05 -0500

    Update RockTunnel1F.asm
    moving zapdos to where steel wing was (and moving steel wing to where elixir was)

--- maps/RockTunnel1F.asm
+++ maps/RockTunnel1F.asm
@@ -1,14 +1,46 @@
 	object_const_def
 	const ROCKTUNNEL1F_POKE_BALL1
-	const ROCKTUNNEL1F_POKE_BALL2
+	const ROCKTUNNEL1F_ZAPDOS
 
 RockTunnel1F_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
+        callback MAPCALLBACK_OBJECTS, RockTunnel1FZapdosCallback
 
-RockTunnel1FElixer:
-	itemball ELIXER
+RockTunnel1FZapdosCallback:
+        checkevent EVENT_FOUGHT_ZAPDOS
+	iftrue .NoAppear
+	checkevent EVENT_OPENED_MT_SILVER
+	iftrue .Appear
+	sjump .NoAppear
+
+.Appear:
+	appear ROCKTUNNEL1F_ZAPDOS
+	endcallback
+
+.NoAppear:
+	disappear ROCKTUNNEL1F_ZAPDOS
+	endcallback
+
+RockTunnel1FZapdos:
+	faceplayer
+	opentext
+	writetext ZapdosText
+	cry ZAPDOS
+	pause 15
+	closetext
+	loadvar VAR_BATTLETYPE, BATTLETYPE_NORMAL
+	loadwildmon ZAPDOS, 50
+	startbattle
+	disappear ROCKTUNNEL1F_ZAPDOS
+	reloadmapafterbattle
+	setevent EVENT_FOUGHT_ZAPDOS
+	end
+
+ZapdosText:
+	text "Gyaoo!"
+	done
 
 RockTunnel1FTMSteelWing:
 	itemball TM_STEEL_WING
@@ -37,5 +69,5 @@ RockTunnel1F_MapEvents:
 	bg_event 21, 15, BGEVENT_ITEM, RockTunnel1FHiddenXDefend
 
 	def_object_events
-	object_event  4, 18, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, RockTunnel1FElixer, EVENT_ROCK_TUNNEL_1F_ELIXER
-	object_event 10, 15, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, RockTunnel1FTMSteelWing, EVENT_ROCK_TUNNEL_1F_TM_STEEL_WING
+	object_event  4, 18, SPRITE_MOLTRES, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, RockTunnel1FZapdos, EVENT_FOUGHT_ZAPDOS
+	object_event 10, 15, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, RockTunnel1FElixer, EVENT_ROCK_TUNNEL_1F_ELIXER
