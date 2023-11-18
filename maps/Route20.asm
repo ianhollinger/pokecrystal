f6aeab38e42bb858518fd75d2b0431385d512dd7
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2023-11-17 21:34:10 -0500

    Update Route20.asm

--- maps/Route20.asm
+++ maps/Route20.asm
@@ -8,7 +8,6 @@ Route20_MapScripts:
 
 	def_callbacks
 	callback MAPCALLBACK_NEWMAP, Route20ClearRocksCallback
-        callback MAPCALLBACK_OBJECTS, Route20ArticunoCallback
 
 Route20ClearRocksCallback:
 	setevent EVENT_CINNABAR_ROCKS_CLEARED
