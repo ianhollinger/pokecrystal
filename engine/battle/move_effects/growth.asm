f75b07f53ce70408ea42d91ef0b96b19e02256a8
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2023-11-28 14:31:18 -0500

    Update growth.asm
    borrowing code from curse to see if I can get growth to display stat messages correctly

--- engine/battle/move_effects/growth.asm
+++ engine/battle/move_effects/growth.asm
@@ -1,12 +1,38 @@
 BattleCommand_Growth:
-	  lb bc, ATTACK, SP_ATTACK
-	  ld a, [wBattleWeather]
-	  cp WEATHER_SUN
-	  jr nz, .got_stats_to_raise
-	  lb bc, ($10 | ATTACK), ($10 | SP_ATTACK)
+; If no stats can be increased, don't.
+; Attack
+	ld a, [ATTACK]
+	cp MAX_STAT_LEVEL
+	jr c, .raise
+; Special Attack
+	ld a, [SP_ATTACK]
+	cp MAX_STAT_LEVEL
+	jr nc, .cantraise
+
+; Raise Attack and Special Attack, +2 in sun
+.raise
+	lb bc, ATTACK, SP_ATTACK
+	ld a, [wBattleWeather]
+	cp WEATHER_SUN
+	jr nz, .got_stats_to_raise
+	lb bc, ($10 | ATTACK), ($10 | SP_ATTACK)
 .got_stats_to_raise
-	  push bc
-	  call RaiseStat
-	  pop bc
-	  ld b, c
-	  jp RaiseStat
+	push bc
+	call RaiseStat
+	call BattleCommand_SwitchTurn
+	call BattleCommand_StatUpMessage
+	call ResetMiss
+	pop bc
+	ld b, c
+	call RaiseStat
+	call BattleCommand_SwitchTurn
+	call BattleCommand_StatUpMessage
+	jp ResetMiss
+
+.cantraise
+; Can't raise either stat.
+	ld b, ABILITY + 1
+	call GetStatName
+	call AnimateFailedMove
+	ld hl, WontRiseAnymoreText
+	jp StdBattleTextbox
