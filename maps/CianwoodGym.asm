1f9e89a60a4a29417a657ffdc674a82752ea7a86
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2023-11-30 00:14:02 -0500

    Update CianwoodGym.asm

--- maps/CianwoodGym.asm
+++ maps/CianwoodGym.asm
@@ -71,8 +71,7 @@ CianwoodGymChuckScript:
 	iftrue .RematchDone
 	checkevent EVENT_OPENED_MT_SILVER
 	iftrue .Rematch
-        checkevent EVENT_OPENED_MT_SILVER
-	iffalse .FightDone
+        sjump .FightDone
 
 .Rematch:
 	writetext ChuckRematchText
