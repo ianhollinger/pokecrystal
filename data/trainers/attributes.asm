d3c7db3416168ab6748a5b32d05ceee4da5c0a67
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2023-12-25 18:07:05 -0500

    Update attributes.asm
    Gave executives items

--- data/trainers/attributes.asm
+++ data/trainers/attributes.asm
@@ -303,7 +303,7 @@ TrainerClassAttributes:
 	dw CONTEXT_USE | SWITCH_SOMETIMES
 
 ; Executivem
-	db NO_ITEM, NO_ITEM ; items
+	db X_DEFEND, NO_ITEM ; items
 	db 18 ; base reward
 	dw AI_BASIC | AI_SETUP | AI_TYPES | AI_SMART | AI_OPPORTUNIST | AI_CAUTIOUS | AI_STATUS | AI_RISKY
 	dw CONTEXT_USE | SWITCH_OFTEN
@@ -327,7 +327,7 @@ TrainerClassAttributes:
 	dw CONTEXT_USE | SWITCH_SOMETIMES
 
 ; Executivef
-	db NO_ITEM, NO_ITEM ; items
+	db X_SP_DEF, NO_ITEM ; items
 	db 18 ; base reward
 	dw AI_BASIC | AI_SETUP | AI_TYPES | AI_SMART | AI_OPPORTUNIST | AI_CAUTIOUS | AI_STATUS | AI_RISKY
 	dw CONTEXT_USE | SWITCH_OFTEN
