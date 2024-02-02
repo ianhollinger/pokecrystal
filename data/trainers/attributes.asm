522e3214bc91564a0dddc27474bda687b552dde2
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2024-02-01 22:09:21 -0500

    Update attributes.asm
    Switched a bunch of x item users around

--- data/trainers/attributes.asm
+++ data/trainers/attributes.asm
@@ -27,7 +27,7 @@ TrainerClassAttributes:
 	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Pryce
-	db X_SP_DEF, NO_ITEM ; items
+	db GUARD_SPEC, NO_ITEM ; items
 	db 25 ; base reward
 	dw AI_BASIC | AI_SETUP | AI_SMART | AI_AGGRESSIVE | AI_CAUTIOUS | AI_STATUS | AI_RISKY
 	dw CONTEXT_USE | SWITCH_OFTEN
@@ -45,7 +45,7 @@ TrainerClassAttributes:
 	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Clair
-	db X_SPECIAL, NO_ITEM ; items
+	db X_SP_DEF, NO_ITEM ; items
 	db 25 ; base reward
 	dw AI_BASIC | AI_SETUP | AI_SMART | AI_AGGRESSIVE | AI_CAUTIOUS | AI_STATUS | AI_RISKY
 	dw CONTEXT_USE | SWITCH_OFTEN
@@ -81,7 +81,7 @@ TrainerClassAttributes:
 	dw CONTEXT_USE | SWITCH_OFTEN
 
 ; Karen
-	db GUARD_SPEC, NO_ITEM ; items
+	db X_SP_DEF, NO_ITEM ; items
 	db 25 ; base reward
 	dw AI_BASIC | AI_SETUP | AI_SMART | AI_AGGRESSIVE | AI_CAUTIOUS | AI_STATUS | AI_RISKY
 	dw CONTEXT_USE | SWITCH_OFTEN
@@ -123,7 +123,7 @@ TrainerClassAttributes:
 	dw CONTEXT_USE | SWITCH_SOMETIMES
 
 ; Erika
-	db X_SP_DEF, NO_ITEM ; items
+	db X_SPECIAL, NO_ITEM ; items
 	db 25 ; base reward
 	dw AI_BASIC | AI_SETUP | AI_SMART | AI_AGGRESSIVE | AI_CAUTIOUS | AI_STATUS | AI_RISKY
 	dw CONTEXT_USE | SWITCH_OFTEN
