585a4af815d47eb66c64d4b31109566a604e02bf
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2024-03-03 23:29:12 -0500

    Update cgb_layouts.asm
    im taking it out

--- engine/gfx/cgb_layouts.asm
+++ engine/gfx/cgb_layouts.asm
@@ -269,14 +269,14 @@ _CGB_Pokedex:
 
 .is_pokemon
 	call GetMonPalettePointer
-	ld a, [wPokedexShinyToggle]
-	and a
-	jr z, .not_shiny
-	inc hl
-	inc hl
-	inc hl
-	inc hl
-.not_shiny
+;	ld a, [wPokedexShinyToggle]
+;	and a
+;	jr z, .not_shiny
+;	inc hl
+;	inc hl
+;	inc hl
+;	inc hl
+;.not_shiny
 	call LoadPalette_White_Col1_Col2_Black ; mon palette
 .got_palette
 	call WipeAttrmap
