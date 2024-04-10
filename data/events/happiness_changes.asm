ac67b52bb0578514cb4f379f23bf6d25a209cad8
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2024-04-09 23:57:24 -0400

    Update happiness_changes.asm

--- data/events/happiness_changes.asm
+++ data/events/happiness_changes.asm
@@ -2,23 +2,23 @@ HappinessChanges:
 ; entries correspond to HAPPINESS_* constants
 	table_width 3, HappinessChanges
 	; change if happiness < 100, change if happiness < 200, change otherwise
-	db  +6,  +4,  +2 ; Gained a level
-	db  +6,  +4,  +2 ; Vitamin
-	db  +3,  +2,  +1 ; X Item
-	db  +3,  +2,  +1 ; Battled a Gym Leader
-	db  +3,  +2,  +1 ; Learned a move
+	db  +5,  +4,  +3 ; Gained a level
+	db  +4,  +3,  +2 ; Vitamin
+	db  +2,  +1,  +0 ; X Item
+	db  +5,  +4,  +3 ; Battled a Gym Leader
+	db  +2,  +1,  +0 ; Learned a move
 	db  -1,  -1,  -1 ; Lost to an enemy
-	db  -3,  -6,  -9 ; Fainted due to poison
-	db  -3,  -6,  -9 ; Lost to a much stronger enemy
+	db  -5,  -5,  -5 ; Fainted due to poison
+	db  -5,  -5,  -5 ; Lost to a much stronger enemy
 	db  +1,  +1,  +1 ; Haircut (older brother) 1
-	db  +3,  +2,  +1 ; Haircut (older brother) 2
-	db  +6,  +4,  +2 ; Haircut (older brother) 3
+	db  +3,  +3,  +3 ; Haircut (older brother) 2
+	db  +5,  +5,  +5 ; Haircut (older brother) 3
 	db  +1,  +1,  +1 ; Haircut (younger brother) 1
-	db  +3,  +2,  +1 ; Haircut (younger brother) 2
-	db  +9,  +6,  +3 ; Haircut (younger brother) 3
-	db  -3,  -6,  -9 ; Used Heal Powder or Energypowder (bitter)
-	db  -6,  -9, -12 ; Used Energy Root (bitter)
-	db -10, -15, -20 ; Used Revival Herb (bitter)
-	db  +3,  +2,  +1 ; Grooming
-	db  +9,  +6,  +3 ; Gained a level in the place where it was caught
+	db  +3,  +3,  +3 ; Haircut (younger brother) 2
+	db +10, +10, +10 ; Haircut (younger brother) 3
+	db  -4,  -8, -12 ; Used Heal Powder or Energypowder (bitter)
+	db  -8, -12, -16 ; Used Energy Root (bitter)
+	db -12, -16, -20 ; Used Revival Herb (bitter)
+	db +10, +10, +10 ; Grooming
+	db +10,  +8,  +6 ; Gained a level in the place where it was caught
 	assert_table_length NUM_HAPPINESS_CHANGES
