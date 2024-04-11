29aceb0c9449d4b0adbaeb18718f6b78169eadcb
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2024-04-11 18:50:51 -0400

    Update Route5.asm
    Level 80 ditto AHHHHH (call it a missingno reference)

--- maps/Route5.asm
+++ maps/Route5.asm
@@ -21,7 +21,7 @@ Route5PokefanMScript:
 	pause 15
 	cry DITTO
 	closetext
-	loadwildmon DITTO, 45
+	loadwildmon DITTO, 80
 	loadvar VAR_BATTLETYPE, BATTLETYPE_SHINY
 	startbattle
 	reloadmapafterbattle
