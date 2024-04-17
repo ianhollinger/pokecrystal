5d81a21aaebb2e043c6fa778822a7862b99ed9c5
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2024-04-16 20:16:42 -0400

    Update dvs.asm
    changing dvs around 
    - making gym leaders closer together
    - giving red his original dvs (not worth nerfing all his mons so charizard can get hp flying)
    - no freaking FFs
    - all unique (I think)

--- data/trainers/dvs.asm
+++ data/trainers/dvs.asm
@@ -6,7 +6,7 @@ TrainerClassDVs:
 	dn  9, 10,  7, 10 ; WHITNEY
 	dn  9, 10, 10,  8 ; BUGSY
 	dn  9,  8, 10, 10 ; MORTY
-	dn  9,  8,  8, 12 ; PRYCE
+	dn 10,  9,  7, 12 ; PRYCE
 	dn  9, 11,  6,  8 ; JASMINE (HP Steel)
 	dn 13,  8,  8,  8 ; CHUCK
 	dn  7, 12, 13, 13 ; CLAIR
@@ -15,19 +15,19 @@ TrainerClassDVs:
 	dn 13, 12, 13, 14 ; WILL
 	dn 13, 13, 12, 12 ; CAL (HP Bug)
 	dn 15, 12, 13, 12 ; BRUNO
-	dn 14, 14, 15, 15 ; KAREN (HP Grass)
+	dn 14, 14, 13, 15 ; KAREN (HP Grass)
 	dn 13, 12, 13, 14 ; KOGA
-	dn 13, 12, 13, 14 ; CHAMPION
+	dn 13, 14, 13, 14 ; CHAMPION
 	dn  9, 12,  8,  8 ; BROCK
 	dn  7,  8, 12,  9 ; MISTY
 	dn 10,  9, 12, 10 ; LT_SURGE (HP Water)
 	dn  7,  8,  9, 10 ; SCIENTIST
-	dn  10, 8, 11, 13 ; ERIKA (HP Fire)
+	dn 10,  8, 11, 11 ; ERIKA (HP Fire)
 	dn  9,  6,  6,  6 ; YOUNGSTER
-	dn  9,  8,  8,  8 ; SCHOOLBOY
+	dn  8, 10, 10, 10 ; SCHOOLBOY
 	dn  9,  8, 10,  6 ; BIRD_KEEPER
 	dn  5,  8,  8,  5 ; LASS
-	dn  7,  8, 10,  7 ; JANINE
+	dn  7, 10, 12,  7 ; JANINE
 	dn 14, 11, 12, 10 ; COOLTRAINERM (HP Electric)
 	dn 10, 15,  9,  9 ; COOLTRAINERF (HP Electric)
 	dn  6,  9,  9,  9 ; BEAUTY
@@ -36,7 +36,7 @@ TrainerClassDVs:
 	dn  8,  9,  9, 10 ; GENTLEMAN
 	dn  7,  6, 10,  9 ; SKIER
 	dn  5,  9,  7,  9 ; TEACHER
-	dn  5,  6, 12, 13 ; SABRINA
+	dn  5,  6, 10, 13 ; SABRINA
 	dn  9,  8,  8,  8 ; BUG_CATCHER
 	dn  7,  6,  7,  8 ; FISHER
 	dn  8,  7,  9,  8 ; SWIMMERM
@@ -47,24 +47,24 @@ TrainerClassDVs:
 	dn  9,  6, 10,  8 ; GUITARIST
 	dn  8, 11,  7,  8 ; HIKER
 	dn  9,  8, 10,  8 ; BIKER
-	dn  9,  8,  8, 12 ; BLAINE
+	dn 10,  7,  7, 12 ; BLAINE
 	dn  9,  8, 10,  8 ; BURGLAR
-	dn  9,  8,  8, 10 ; FIREBREATHER
+	dn  7,  8,  9, 10 ; FIREBREATHER
 	dn 10,  6, 13,  6 ; JUGGLER (HP Grass)
 	dn 12,  7,  7,  8 ; BLACKBELT_T
 	dn 10, 10, 13, 14 ; EXECUTIVEM (HP Grass)
-	dn  9,  8,  8, 10 ; PSYCHIC_T (HP Rock)
+	dn  9,  6,  8, 10 ; PSYCHIC_T (HP Ghost)
 	dn  8,  8,  9,  9 ; PICNICKER
 	dn  7,  6,  8,  8 ; CAMPER
 	dn 10, 10, 13, 13 ; EXECUTIVEF
-	dn  9,  8,  8,  8 ; SAGE (HP Rock)
-	dn  7,  8,  8, 10 ; MEDIUM
+	dn  9,  8,  8, 10 ; SAGE (HP Rock)
+	dn  7,  8,  6, 11 ; MEDIUM
 	dn  7,  6, 11,  8 ; BOARDER
 	dn  9,  7,  5,  8 ; POKEFANM
-	dn  5,  8,  8, 10 ; KIMONO_GIRL
+	dn  7,  8,  8,  9 ; KIMONO_GIRL
 	dn  5,  5,  5,  5 ; TWINS
 	dn  7,  9,  5,  7 ; POKEFANF
-	dn 12, 13, 15, 15 ; RED
+	dn 15, 13, 13, 14 ; RED
 	dn 13, 13,  9, 12 ; BLUE
 	dn  7,  7,  8,  8 ; OFFICER
 	dn  7, 14, 10,  7 ; GRUNTF
