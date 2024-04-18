ecb62234aa07c6dd62157c8f8d874856b39bd847
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2024-04-17 21:28:59 -0400

    Update Route45.asm
    quentin SHOULD only appear after beating the radio tower

--- maps/Route45.asm
+++ maps/Route45.asm
@@ -17,6 +17,16 @@ Route45_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
+        callback MAPCALLBACK_OBJECTS, Route45CamperQuentinCallback
+
+Route45CamperQuentinCallback:
+	checkevent EVENT_CLEARED_RADIO_TOWER
+	iftrue .NoAppear
+	appear ROUTE45_YOUNGSTER
+	endcallback
+.NoAppear:
+	disappear ROUTE45_YOUNGSTER
+	endcallback
 
 TrainerBlackbeltKenji:
 	trainer BLACKBELT_T, KENJI3, EVENT_BEAT_BLACKBELT_KENJI, BlackbeltKenji3SeenText, BlackbeltKenji3BeatenText, 0, .Script
