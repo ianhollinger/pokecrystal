b3ffde257c82f58e3181c4614d3cc6191cb0eb4c
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2024-07-05 11:48:07 -0400

    Update dvs.asm
    babe new dvs just dropped

--- data/trainers/dvs.asm
+++ data/trainers/dvs.asm
@@ -12,7 +12,7 @@ TrainerClassDVs:
 	dn  7, 12, 13, 13 ; CLAIR
 	dn 14, 13, 13, 14 ; RIVAL1
 	dn  9,  8,  8,  8 ; POKEMON_PROF
-	dn 13, 12, 13, 14 ; WILL
+	dn 13, 12, 15, 12 ; WILL
 	dn 13, 13, 12, 12 ; CAL (HP Bug)
 	dn 15, 12, 13, 12 ; BRUNO
 	dn 14, 14, 13, 15 ; KAREN (HP Grass)
@@ -23,49 +23,49 @@ TrainerClassDVs:
 	dn 10,  9, 12, 10 ; LT_SURGE (HP Water)
 	dn  7,  8,  9, 10 ; SCIENTIST
 	dn 10,  8, 11, 11 ; ERIKA (HP Fire)
-	dn  9,  6,  6,  6 ; YOUNGSTER
-	dn  8, 10, 10, 10 ; SCHOOLBOY
+	dn  8,  7,  7,  6 ; YOUNGSTER
+	dn  7,  8,  6,  6 ; SCHOOLBOY
 	dn  9,  8, 10,  6 ; BIRD_KEEPER
 	dn  5,  8,  8,  5 ; LASS
 	dn  7, 10, 12,  7 ; JANINE
 	dn 10, 15,  9, 10 ; COOLTRAINERM (HP Electric)
 	dn 14, 11,  8, 11 ; COOLTRAINERF (HP Electric)
-	dn  6,  9,  9,  9 ; BEAUTY
+	dn  7,  5,  7,  5 ; BEAUTY
 	dn  9,  7,  7,  8 ; POKEMANIAC
-	dn 13,  8, 10,  8 ; GRUNTM
-	dn  8,  9,  9, 10 ; GENTLEMAN
+	dn 12, 10,  9, 10 ; GRUNTM
+	dn  9,  8,  9,  8 ; GENTLEMAN
 	dn  7,  6, 10,  9 ; SKIER
 	dn  5,  9,  7,  9 ; TEACHER
 	dn  5,  6, 10, 13 ; SABRINA
-	dn  9,  8,  8,  8 ; BUG_CATCHER
+	dn  7,  8,  7,  6 ; BUG_CATCHER
 	dn  7,  6,  7,  8 ; FISHER
-	dn  8,  7,  9,  8 ; SWIMMERM
-	dn  5,  6,  7,  9 ; SWIMMERF
-	dn  9,  7,  6,  8 ; SAILOR
+	dn  9,  6,  7,  6 ; SWIMMERM
+	dn  7,  6,  8,  7 ; SWIMMERF
+	dn 10,  9,  6,  8 ; SAILOR
 	dn  9,  7,  9,  6 ; SUPER_NERD
 	dn 14, 13, 13, 14 ; RIVAL2 (HP Water)
 	dn  9,  6, 10,  8 ; GUITARIST
 	dn  8, 11,  7,  8 ; HIKER
-	dn  9,  8, 10,  8 ; BIKER
+	dn  8,  7,  9,  8 ; BIKER
 	dn 10,  7,  7, 12 ; BLAINE
-	dn 10,  7, 11,  8 ; BURGLAR
-	dn  7,  8,  9, 10 ; FIREBREATHER
-	dn 10,  6, 13,  6 ; JUGGLER (HP Grass)
+	dn 10,  7, 11,  6 ; BURGLAR
+	dn  8,  8,  9, 10 ; FIREBREATHER
+	dn  6,  6, 13,  8 ; JUGGLER (HP Grass)
 	dn 12,  7,  7,  8 ; BLACKBELT_T
 	dn 11,  9, 10, 10 ; EXECUTIVEM (HP Ice)
 	dn  9,  6,  8, 10 ; PSYCHIC_T (HP Ghost)
-	dn  8,  8,  9,  9 ; PICNICKER
-	dn  7,  6,  8,  8 ; CAMPER
+	dn  6,  9,  8,  7 ; PICNICKER
+	dn  8,  9,  7,  6 ; CAMPER
 	dn 10, 10, 13, 13 ; EXECUTIVEF
 	dn  9,  8,  8, 10 ; SAGE (HP Rock)
 	dn  7,  8,  6, 11 ; MEDIUM
 	dn  7,  6, 11,  8 ; BOARDER
-	dn  9,  7,  5,  8 ; POKEFANM
-	dn  7,  8,  8,  9 ; KIMONO_GIRL
+	dn  9,  7,  6,  8 ; POKEFANM
+	dn  5,  9,  6,  9 ; KIMONO_GIRL
 	dn  5,  5,  5,  5 ; TWINS
 	dn  7,  9,  5,  7 ; POKEFANF
 	dn 15, 13, 13, 14 ; RED
-	dn 13, 13,  9, 12 ; BLUE
+	dn 13, 12, 12, 12 ; BLUE
 	dn  7,  7,  8,  8 ; OFFICER
-	dn  7, 14, 10,  7 ; GRUNTF
+	dn  8, 10,  8, 13 ; GRUNTF
 	dn 11, 10, 10, 10 ; MYSTICALMAN (shiny)
