a4c6f456e71daf1f336531f606259bcab378f8a7
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2024-08-19 22:43:13 -0400

    Update MahoganyRedGyaradosSpeechHouse.asm
    idk

--- maps/MahoganyRedGyaradosSpeechHouse.asm
+++ maps/MahoganyRedGyaradosSpeechHouse.asm
@@ -11,7 +11,6 @@ MahoganyRedGyaradosSpeechHouseBlackBeltScript:
 	jumptextfaceplayer MahoganyRedGyaradosSpeechHouseBlackBeltText
 
 MahoganyRedGyaradosSpeechHouseTeacherScript:
-	setmapscene MAHOGANY_TOWN, SCENE_MAHOGANYTOWN_NOOP
 	faceplayer
 	opentext
 	checkflag ENGINE_ROCKETS_IN_RADIO_TOWER
