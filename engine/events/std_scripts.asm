6f74952fb7e6a366e49f76a8114a8f1ab0a4d1cd
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2024-10-05 13:36:29 -0400

    Update std_scripts.asm
    Coins cost half as much (¥10)

--- engine/events/std_scripts.asm
+++ engine/events/std_scripts.asm
@@ -1828,10 +1828,10 @@ CoinVendor_IntroScript:
 .Buy50:
 	checkcoins MAX_COINS - 50
 	ifequal HAVE_MORE, .CoinCaseFull
-	checkmoney YOUR_MONEY, 1000
+	checkmoney YOUR_MONEY, 500
 	ifequal HAVE_LESS, .NotEnoughMoney
 	givecoins 50
-	takemoney YOUR_MONEY, 1000
+	takemoney YOUR_MONEY, 500
 	waitsfx
 	playsound SFX_TRANSACTION
 	farwritetext CoinVendor_Buy50CoinsText
@@ -1841,10 +1841,10 @@ CoinVendor_IntroScript:
 .Buy500:
 	checkcoins MAX_COINS - 500
 	ifequal HAVE_MORE, .CoinCaseFull
-	checkmoney YOUR_MONEY, 10000
+	checkmoney YOUR_MONEY, 5000
 	ifequal HAVE_LESS, .NotEnoughMoney
 	givecoins 500
-	takemoney YOUR_MONEY, 10000
+	takemoney YOUR_MONEY, 5000
 	waitsfx
 	playsound SFX_TRANSACTION
 	farwritetext CoinVendor_Buy500CoinsText
