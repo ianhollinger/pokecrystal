c1b72b9f76209907c13e2f7a9f505c489883f22f
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2024-11-23 15:36:17 -0500

    Update MountMortar2FInside.asm
    replace defense curl with substitute

--- maps/MountMortar2FInside.asm
+++ maps/MountMortar2FInside.asm
@@ -30,7 +30,7 @@ MountMortar2FInsideRareCandy:
 	itemball RARE_CANDY
 
 MountMortar2FInsideTMDefenseCurl:
-	itemball TM_DEFENSE_CURL
+	itemball TM_SUBSTITUTE
 
 MountMortar2FInsideDragonScale:
 	itemball DRAGON_SCALE
