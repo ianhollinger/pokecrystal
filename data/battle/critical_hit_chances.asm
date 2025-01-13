b6f5939ea1bd40cb16073e3bce187e35416b1284
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2025-01-13 11:13:23 -0500

    Update critical_hit_chances.asm
    Making +3 rate 100% to test if focus energy is broken (+5 should always crit but is only 33% of time)

--- data/battle/critical_hit_chances.asm
+++ data/battle/critical_hit_chances.asm
@@ -2,7 +2,7 @@ CriticalHitChances:
 	db 6 percent + 1   ;  0 
 	db 12 percent + 1  ; +1 
 	db 25 percent      ; +2 
-	db 50 percent      ; +3 
+	db 100 percent     ; +3 
 	db 100 percent     ; +4
 	db 100 percent     ; +5
 	db 100 percent     ; +6
