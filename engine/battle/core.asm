71f5acb40ee77c9b0be0d7a5451d4a07472faad2
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2025-04-29 19:20:57 -0400

    Update core.asm
    Nerf sandstorm lmao

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -1779,7 +1779,7 @@ HandleWeather:
 	ld de, ANIM_IN_SANDSTORM
 	call Call_PlayBattleAnim
 	call SwitchTurnCore
-	call GetEighthMaxHP
+	call GetSixteenthMaxHP
 	call SubtractHPFromUser
 
 	ld hl, SandstormHitsText
