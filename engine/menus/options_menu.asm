dc54f6ed4690c12e4a79681570e98a793e83ca38
ianhollinger <118033144+ianhollinger@users.noreply.github.com>
2025-06-13 18:00:05 -0400

    Update options_menu.asm
    now it goes mid>slow>fast

--- engine/menus/options_menu.asm
+++ engine/menus/options_menu.asm
@@ -178,17 +178,17 @@ GetTextSpeed:
 	jr z, .slow
 	; none of the above
 	ld c, OPT_TEXT_SPEED_MED
-	lb de, TEXT_DELAY_SLOW, TEXT_DELAY_FAST 
+	lb de, TEXT_DELAY_FAST, TEXT_DELAY_SLOW
 	ret
 
 .fast
 	ld c, OPT_TEXT_SPEED_FAST 
-	lb de, TEXT_DELAY_MED, TEXT_DELAY_SLOW 
+	lb de, TEXT_DELAY_SLOW, TEXT_DELAY_MED 
 	ret
 
 .slow
 	ld c, OPT_TEXT_SPEED_SLOW 
-	lb de, TEXT_DELAY_FAST, TEXT_DELAY_MED
+	lb de, TEXT_DELAY_MED, TEXT_DELAY_FAST
 	ret
 
 Options_BattleScene:
@@ -526,17 +526,17 @@ GetDifficulty:
 	jr z, .easy
 	; none of the above
 	ld c, OPT_DIFFICULTY_NORMAL
-	lb de, DIFFICULTY_EASY, DIFFICULTY_HARD
+	lb de, DIFFICULTY_HARD, DIFFICULTY_EASY
 	ret
 
 .hard
 	ld c, OPT_DIFFICULTY_HARD
-	lb de, DIFFICULTY_NORMAL, DIFFICULTY_EASY
+	lb de, DIFFICULTY_EASY, DIFFICULTY_NORMAL
 	ret
 
 .easy
 	ld c, OPT_DIFFICULTY_EASY
-	lb de, DIFFICULTY_HARD, DIFFICULTY_NORMAL
+	lb de, DIFFICULTY_NORMAL, DIFFICULTY_HARD
 	ret
 
 Options_Frame:
